15cf67c9796689781bed89cd4e940057b3b45271
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-07-29 07:57:14 -0400

    Add seed data for donations

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(
+user1 = User.create(
   email: "user1@example.com",
   password: "password",
   annual_income: 100000.00,
   pledge_percentage: 0.10
 )
 
-User.create(
+user2 = User.create(
   email: "user2@example.com",
   password: "password",
   annual_income: 130000.00,
   pledge_percentage: 0.01
 )
+
+user1.donations.create(amount: 1000.00)
+user1.donations.create(amount: 2000.00)
+
+user2.donations.create(amount: 10.00)
+user2.donations.create(amount: 11.00)
+user2.donations.create(amount: 10.20)
