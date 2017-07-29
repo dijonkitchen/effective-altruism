2cda5b78263161a6ecd583568f44406db2317f84
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-07-29 08:57:40 -0400

    Make Users component showing User data

--- client/app/bundles/HelloWorld/components/HelloWorld.jsx
+++ client/app/bundles/HelloWorld/components/HelloWorld.jsx
@@ -2,10 +2,6 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 export default class HelloWorld extends React.Component {
-  static propTypes = {
-    name: PropTypes.string.isRequired, // this is passed from the Rails view
-  };
-
   /**
    * @param props - Comes from your rails view.
    * @param _railsContext - Comes from React on Rails
@@ -15,31 +11,43 @@ export default class HelloWorld extends React.Component {
 
     // How to set initial state in ES6 class syntax
     // https://facebook.github.io/react/docs/reusable-components.html#es6-classes
-    this.state = { name: this.props.name };
+    this.state = { users: [] };
   }
 
-  updateName = (name) => {
-    this.setState({ name });
-  };
+  componentDidMount() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    fetch('/users')
+      .then(users => users.json())
+      .then(responseJSON => this.setState({users: responseJSON}))
+  }
+
+  renderUsers(users) {
+    return users.map((user) => {
+      return (
+        <li
+          key={user.id}
+        >
+          <ul>
+            <li>{user.email}</li>
+            <li>Income: {user.annual_income}</li>
+            <li>Pledge %: {user.pledge_percentage * 100}</li>
+            <li>Total donations: {user.donated}</li>
+            <li>Left to donate: {user.left_to_donate}</li>
+          </ul>
+        </li>
+      )
+    })
+  }
 
   render() {
     return (
       <div>
-        <h3>
-          Hello, {this.state.name}!
-        </h3>
-        <hr />
-        <form >
-          <label htmlFor="name">
-            Say hello to:
-          </label>
-          <input
-            id="name"
-            type="text"
-            value={this.state.name}
-            onChange={(e) => this.updateName(e.target.value)}
-          />
-        </form>
+        <ul>
+          {this.renderUsers(this.state.users)}
+        </ul>
       </div>
     );
   }
