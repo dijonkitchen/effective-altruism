d7869a9396153bfff622ec19e59cb53932b62051
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-10-23 20:59:04 -0700

    JS test

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {users: []}
+
+  componentDidMount() {
+    fetch('/api/users')
+      .then(res => {console.log(res); res.json()})
+      .then(users => console.log(users));
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Users</h1>
+        {this.state.users.map(user =>
+          <div key={user.id}>{user.username}</div>
+        )}
       </div>
     );
   }
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/api/users', function(req, res, next) {
+  let users = {
+    id: 2,
+    username: "laksdjf",
+  }
+  res.send(JSON.stringify(users));
 });
 
 module.exports = router;
